7010f4ba9e76cf5db214ff947ea4b24cd171d48d
mcramileux <kristineramilo21@gmail.com>
2023-08-05 06:56:30 +1000

    update contact stil incomplete

--- mcramileux-portfolio/src/components/pages/Contact.js
+++ mcramileux-portfolio/src/components/pages/Contact.js
@@ -1,24 +1,102 @@
 //TO EDIT presented with the Contact section
 //a contact form with fields for a name, an email address, and a message
-import React from 'react';
+import React, { useState} from 'react';
+import '.src/assets/css/styles.css';
+
+// Here we import a helper function that will check if the email is valid
+import { checkPassword, validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
-  return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
-  );
-}
+
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorName, setErrorName] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  
+
+  const handleNameChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+        setEmail(e.target.value);
+        setEmailError(false);
+      };
+      
+  const handleMessageChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+        setMessage(e.target.value);
+        setMessageError(false);
+  };
+  
+
+    // Based on the input type, we set the state of either name, email, or message
+    if (inputType === 'name' && !inputValue) {
+      setErrorMessage('Name is required!');
+    } else if (inputType === 'email' && !inputValue) {
+      setErrorMessage('Email is required!');
+    } else if (inputType === 'message' && !inputValue) {
+      setErrorMessage('Message is required!');
+    }
+  };
+  const handleSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email)) {
+        setErrorMessage('Email is invalid');
+        return;
+      }
+    if (!checkPassword(password)) {
+        setErrorMessage(
+          `Choose a more secure password for the account`);
+        return;
+    }
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName('');
+    setPassword('');
+    setEmail('');
+    setErrorMessage('');
+  };
+
+  //TO EDIT all below
+    return (
+        <div>
+          <form className="form">
+            <input
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              onMessage={handleMessageChange}
+              type="email"
+              placeholder="email"
+            />
+            <input
+              value={userName}
+              name="userName"
+              onChange={handleInputChange}
+              type="text"
+              placeholder="username"
+            />
+            <input
+              value={password}
+              name="password"
+              onChange={handleInputChange}
+              type="password"
+              placeholder="password"
+            />
+            <button type="button" onClick={handleFormSubmit}>Submit</button>
+          </form>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+        </div>
+      );
+    }
+
+    export default Contact;
