b9327363ffe10a9f368bbce38a6a642e6a4dc060
mcramileux <kristineramilo21@gmail.com>
2023-08-07 00:30:06 +1000

    navtabs updated

--- mcramileux-portfolio/src/components/NavTabs.js
+++ mcramileux-portfolio/src/components/NavTabs.js
@@ -5,63 +5,68 @@
 // WHEN I click on a navigation title
 // THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
 
-import React from "react";
+import React from 'react';
+import './NavTabs.css';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <header>
-      <h1 href="#about" onClick={() => handlePageChange("About")}>
+    <nav className='navbar navbar-expand-md'>
+      <div className="container-nav">
+      {/* <h1 href='#about' onClick={() => handlePageChange('About')}>
         Kristine Ramilo
-      </h1>
-      <nav>
-        <ul>
-          <li className="nav-item">
+      </h1> */}
+      <h1 className='author'>Kristine Ramilo</h1>
+      <div className='navbar-collapse'>
+        <ul className='navbar-nav ms-auto'>
+          <li className='nav-item'>
             <a
-              href="#about"
-              onClick={() => handlePageChange("About")}
+              href='#about'
+              onClick={() => handlePageChange('About')}
               className={
-                currentPage === "About" ? "nav-link active" : "nav-link"
+                currentPage === 'About' ? 'nav-link active' : 'nav-link'
               }
             >
               About
             </a>
           </li>
-          <li className="nav-item">
+          <li className='nav-item'>
             <a
-              href="#Portfolio"
-              onClick={() => handlePageChange("Portfolio")}
+              href='#Portfolio'
+              onClick={() => handlePageChange('Portfolio')}
               className={
-                currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+                currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'
               }
             >
               Portfolio
             </a>
           </li>
-          <li className="nav-item">
+          <li className='nav-item'>
             <a
-              href="#contact"
-              onClick={() => handlePageChange("Contact")}
+              href='#contact'
+              onClick={() => handlePageChange('Contact')}
               className={
-                currentPage === "Contact" ? "nav-link active" : "nav-link"
+                currentPage === 'Contact' ? 'nav-link active' : 'nav-link'
               }
             >
               Contact
             </a>
           </li>
-          <li className="nav-item">
+          <li className='nav-item'>
             <a
-              href="#resume"
-              onClick={() => handlePageChange("Resume")}
+              href='#resume'
+              onClick={() => handlePageChange('Resume')}
               className={
-                currentPage === "Resume" ? "nav-link active" : "nav-link"
+                currentPage === 'Resume' ? 'nav-link active' : 'nav-link'
               }
             >
-              Resumé
+              Resume
             </a>
           </li>
         </ul>
+        </div>
+      </div>
       </nav>
-    </header>
+    
   );
 }
 
