3702292e91f578e883d1b2adc361e7e10a771aec
mcramileux <kristineramilo21@gmail.com>
2023-08-07 02:06:06 +1000

    footer updated except logo wont appear

--- mcramileux-portfolio/src/components/Footer.js
+++ mcramileux-portfolio/src/components/Footer.js
@@ -4,30 +4,32 @@
 //and their profile on a third platform (Stack Overflow, Twitter)
 
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faGithub, faLinkedin } from '@fortawesome/free-solid-svg-icons';
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-light py-3">
-      <div className="footerContainer">
-        <div className="icons">
-            <a href="https://github.com/mcramileux/" target="_blank" rel="noopener noreferrer">
-                {/* <FontAwesomeIcon icon={faGithub} /> */} Github
-            </a>
-            <a href="https://www.linkedin.com/in/mcramileux/" target="_blank" rel="noopener noreferrer">
-                {/* <FontAwesomeIcon icon={faLinkedin} /> */} Linkedin
-            </a>
-            <a href="https://twitter.com/mcramileux" target="_blank" rel="noopener noreferrer">
-                {/* <FontAwesomeIcon icon={faTwitter} />    */} Twitter
-            </a>
+    <footer className="bg-dark text-info py-3">
+      <div className="container-footer text-center">
+        <div className="row">
+          <div className="col-12">
+            <div className="social-icons">
+              <a href="https://github.com/mcramileux/" target="_blank" className="text-info" rel="noopener noreferrer">
+                 {/* <FontAwesomeIcon icon={faGithub} />  */}Github
+                 
+              </a>
+              <a href="https://www.linkedin.com/in/mcramileux/" target="_blank" className="text-info" rel="noopener noreferrer">
+                    {/* <FontAwesomeIcon icon={faLinkedin} /> */} Linkedin
+              </a>
+            </div>
+              <section>
+                <p>
+                  <h6 className="copyright-text">&copy; 2023 mcramileux. All rights reserved.</h6>
+                </p>
+              </section> 
+          </div>
         </div>
-        <section>
-        <h5>Made with ❤️️ by Kristine</h5>
-        <p>
-           <h6 className="copyright-text">&copy; 2023 mcramileux. All rights reserved.</h6>
-        </p>
-        </section> 
       </div>
     </footer>
   );
