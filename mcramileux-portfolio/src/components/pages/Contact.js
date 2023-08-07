ba6ff323e8e81b96049e7720792275c8dd79d4b1
mcramileux <kristineramilo21@gmail.com>
2023-08-07 23:36:08 +1000

    updated contact.css

--- mcramileux-portfolio/src/components/pages/Contact.js
+++ mcramileux-portfolio/src/components/pages/Contact.js
@@ -4,70 +4,136 @@ import React, { useState} from 'react';
 // import '../../css/styles.css';
 import './Contact.css';
 
- function Contact() {
+export default function Contact() {
 
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [errorName, setErrorName] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [sentMessage, setMessageSent] = useState(false);
   
   const handleNameChange = (e) => {
-    // Getting the value and name of the input which triggered the change
         setEmail(e.target.value);
-      };
-      
-  const handleMessageChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-        setMessage(e.target.value);
+        setErrorName(false);
   };
   
-  const handleSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
+  const handleMessageChange = (e) => {
+        setMessage(e.target.value);
+        setErrorMessage(false);
+  };
 
+  const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        setErrorEmail(false);
+  };
+  
+  // Validate the email provide using regex
+  const validateEmail = (email) => {
+    return /^[\w\.-]+@[\w\.-]+\.\w+$/.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let valid = true;
+
+    if (name.length <= 3) {
+      setErrorName(true);
+      valid = false;
+    }
+
+    if (!validateEmail(email)) {
+      setErrorEmail(true);
+      valid = false;
+    }
+
+    if (message.length < 30) {
+      setErrorMessage(true);
+      valid = false;
+    }
+
+    if (valid) {
     // If everything goes according to plan, we want to clear out the input after a successful registration.
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
     setName('');
     setMessage('');
     setEmail('');
+    setMessageSent(true);
   }
+};
+
     return (
       <section id='contact'>
         <div className='container-contact text-center'>
-          <h1 className='contact-title'>Contact Me</h1>
-            <form className='form'>
-              <input
-              value={name}
-              name='name'
-              onChange={handleNameChange}
-              type='name'
-              placeholder='name'
-              />
-              <input
-              value={email}
-              name='email'
-              onChange={handleNameChange}
-              onMessage={handleMessageChange}
-              type='email'
-              placeholder='accountname@email.com'
-              />
-              <input
-              value={message}
-              name='message'
-              onChange={handleNameChange}
-              type='message'
-              placeholder='please type your message'
-              />
-              <button type='submit' onClick={handleSubmit}>Submit</button>
+          <div className='row'>
+            <div className="col-md-12">
+
+              <h1 className='contact-title'>Contact Me</h1>
+                {sentMessage && (
+                  <div className="alert alert-success" role="alert">
+                    "Thank you for contacting me! I have received your message and will get back to you as soon as possible."
+                  </div>
+                )}
+                
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                      <input
+                        type="text"
+                        className={`form-control ${errorName ? 'is-invalid' : ''}`}
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(handleNameChange)}
+                      />
+                        {errorName && (
+                         <div className="invalid-feedback">Please enter your name</div>
+                        )}
+                        </div>
+
+                <div className="form-group">
+                <label htmlFor="email">Email Address:</label>
+                <input
+                  type="email"
+                  className={`form-control ${errorEmail ? 'is-invalid' : ''}`}
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(handleEmailChange)}
+                />
+                  {errorEmail && (
+                  <div className="invalid-feedback">Please enter a valid email address</div>
+                  )}
+                </div>
+              
+                <div className="form-group">
+                <label htmlFor="message">Message:</label>
+                <textarea
+                className={`form-control ${errorMessage ? 'is-invalid' : ''}`}
+                id="message"
+                rows="5"
+                value={message}
+                onChange={(e) => setMessage(handleMessageChange)}
+                ></textarea>
+                {errorMessage && (
+                  <div className="invalid-feedback"> Message must be at least 30 characters</div>
+                )}
+                </div>
+
+                {/* <button type='submit' onClick={handleSubmit}>Submit</button> */}
+                <button type="submit" className="btn btn-info">
+                Submit
+              </button>
             </form>
-          {/* {errorMessage && (
-            <div>
-              <p className="error-text">{errorMessage}</p>
+            {/* {setMessageSent && (
+              <div className="alert alert-success mt-3" role="alert">
+                Your message has been sent. Thank you!
+              </div>
+            )} */}
             </div>
-          )} */}
-          </div>
+        </div>
+        </div>
       </section>
       );
     }
-
-    export default Contact;
