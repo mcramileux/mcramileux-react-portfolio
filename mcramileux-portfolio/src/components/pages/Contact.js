3f7a4aa4d77c1105b38b49b5e990568d69ed1506
mcramileux <kristineramilo21@gmail.com>
2023-08-08 14:27:01 +1000

    contact form almost complted

--- mcramileux-portfolio/src/components/pages/Contact.js
+++ mcramileux-portfolio/src/components/pages/Contact.js
@@ -1,16 +1,16 @@
 //TO EDIT presented with the Contact section
-//a contact form with fields for a name, an email address, and a message
+//a contact form with fields for a name, an email address, and a message ---DONE
 
 // WHEN I am presented with the Contact section
-// THEN I see a contact form with fields for a name, an email address, and a message
+// THEN I see a contact form with fields for a name, an email address, and a message ---DONE
 
 // WHEN I move my cursor out of one of the form fields without entering text
+// THEN I receive a notification that this field is required ---DONE
+
+// WHEN I move my cursor out of one of the form fields without entering text ---DONE
 // THEN I receive a notification that this field is required
 
-// WHEN I move my cursor out of one of the form fields without entering text
-// THEN I receive a notification that this field is required
-
-// WHEN I enter text into the email address field
+// WHEN I enter text into the email address field ---DONE
 // THEN I receive a notification if I have entered an invalid email address
 
 
@@ -29,16 +29,19 @@ export default function Contact() {
   const [errorMessage, setErrorMessage] = useState(false);
   const [sentMessage, setMessageSent] = useState(false);
   
+  // Name in the input changes
   const handleNameChange = (e) => {
-        setEmail(e.target.value);
+        setName(e.target.value);
         setErrorName(false);
   };
   
+  // Message in the input changes
   const handleMessageChange = (e) => {
         setMessage(e.target.value);
         setErrorMessage(false);
   };
 
+  // Email in the input changes
   const handleEmailChange = (e) => {
         setEmail(e.target.value);
         setErrorEmail(false);
@@ -49,6 +52,7 @@ export default function Contact() {
     return /^[\w\.-]+@[\w\.-]+\.\w+$/.test(email);
   };
 
+  // Submit the form
   const handleSubmit = (e) => {
     e.preventDefault();
     let valid = true;
@@ -81,72 +85,74 @@ export default function Contact() {
     return (
       <section id='contact'>
         <div className='container-contact text-center'>
-          <div className='row'>
-            <div className="col-md-12">
-
+          <div className='row justify-content-center'>
               <h1 className='contact-title'>Contact Me</h1>
-                {sentMessage && (
+               <div className="row gx-4 gx-lg-5 justify-content-center mb-5">
+               <div className="col-md-6 mx-auto">
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="name">Name:</label>
+                        <div class="form-floating mb-3">
+                        <input
+                          type="text"
+                          className={`form-control ${errorName ? 'is-invalid' : ''}`}
+                          id="name"
+                          value={name}
+                          onChange={handleNameChange}
+                        />
+                          {errorName && (
+                          <div className="invalid-feedback">Please enter your name</div>
+                          )}
+                          </div>
+                      </div>
+
+                  <div className="form-group">
+                    <label htmlFor="email">Email Address:</label>
+                    <div class="form-floating mb-3">
+                      <input
+                        type="email"
+                        className={`form-control ${errorEmail ? 'is-invalid' : ''}`}
+                        id="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                      />
+                        {errorEmail && (
+                        <div className="invalid-feedback">Please enter a valid email address</div>
+                        )}
+                      </div>
+                    </div>
+              
+                   <div className="form-group">
+                    <label htmlFor="message">Message:</label>
+                    <div class="form-floating mb-3">
+                      <textarea
+                      className={`form-control ${errorMessage ? 'is-invalid' : ''}`}
+                      id="message"
+                      rows="5"
+                      value={message}
+                      onChange={handleMessageChange}
+                      ></textarea>
+                        {errorMessage && (
+                        <div className="invalid-feedback"> Message must be at least 30 characters</div>
+                        )}
+                        </div>
+                    </div>
+                      {/* <button type='submit' onClick={handleSubmit}>Submit</button> */}
+                      <button type="submit" className="btn btn-info">
+                      Submit
+                      </button>
+                  </form>
+                  {sentMessage && (
                   <div className="alert alert-success" role="alert">
                     "Thank you for contacting me! I have received your message and will get back to you as soon as possible."
                   </div>
                 )}
                 
-                <form onSubmit={handleSubmit}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                      <input
-                        type="text"
-                        className={`form-control ${errorName ? 'is-invalid' : ''}`}
-                        id="name"
-                        value={name}
-                        onChange={(e) => setName(handleNameChange)}
-                      />
-                        {errorName && (
-                         <div className="invalid-feedback">Please enter your name</div>
-                        )}
-                        </div>
-
-                <div className="form-group">
-                <label htmlFor="email">Email Address:</label>
-                <input
-                  type="email"
-                  className={`form-control ${errorEmail ? 'is-invalid' : ''}`}
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(handleEmailChange)}
-                />
-                  {errorEmail && (
-                  <div className="invalid-feedback">Please enter a valid email address</div>
-                  )}
-                </div>
-              
-                <div className="form-group">
-                <label htmlFor="message">Message:</label>
-                <textarea
-                className={`form-control ${errorMessage ? 'is-invalid' : ''}`}
-                id="message"
-                rows="5"
-                value={message}
-                onChange={(e) => setMessage(handleMessageChange)}
-                ></textarea>
-                {errorMessage && (
-                  <div className="invalid-feedback"> Message must be at least 30 characters</div>
-                )}
-                </div>
-
-                {/* <button type='submit' onClick={handleSubmit}>Submit</button> */}
-                <button type="submit" className="btn btn-info">
-                Submit
-              </button>
-            </form>
-            {/* {setMessageSent && (
-              <div className="alert alert-success mt-3" role="alert">
-                Your message has been sent. Thank you!
-              </div>
-            )} */}
-            </div>
+          </div>
         </div>
-        </div>
-      </section>
-      );
-    }
+      </div>
+      </div>
+    
+    </section>
+  );
+}
