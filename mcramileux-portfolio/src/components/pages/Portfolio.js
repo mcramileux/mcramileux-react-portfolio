54e7e6a8275d5c4cc8d6adaeb2703ab0f260ecf3
mcramileux <kristineramilo21@gmail.com>
2023-08-09 11:26:56 +1000

    portfolio not showing

--- mcramileux-portfolio/src/components/pages/Portfolio.js
+++ mcramileux-portfolio/src/components/pages/Portfolio.js
@@ -12,74 +12,74 @@
 // A link to the deployed application
 // A link to the corresponding GitHub repository
 
-// import React from 'react';
-// import Project from './Project';
+import React from 'react';
+import Project from './Project.js';
 // import './Portfolio.css';
 
-// import BloomAndBarter from '../../assets/images/bloom-and-barter.png';
-// import MvcTechBlog from '../../assets/images/mvc-tech-blog.png';
-// import McramileuxCssPortfolio from '../..assets/images/mcramileux-css-portfolio.png';
-// import NoteTaker from '../../assets/images/note-taker.png';
-// import DailyScheduler from '../../assets/images/daily-scheduler.png';
-// import WeatherDashboard from '../../assets/images/weather-dashboard.png';
+import BloomAndBarter from '../../assets/images/BloomAndBarter.png';
+import MvcTechBlog from '../../assets/images/MvcTechBlog.png';
+import firstPortfolio from '../..assets/images/firstPortfolio.png';
+import NoteTaker from '../../assets/images/NoteTaker.png';
+import DailyScheduler from '../../assets/images/DailyScheduler.png';
+import WeatherDashboard from '../../assets/images/WeatherDashboard.png';
 
-// export default function Portfolio() {
-//   return (
-//     <section id="Portfolio">
-//       <div className='container-portfolio text-center'>
-//         <h1 className='portfolio-title'>Portfolio</h1>
-//           <div className='container'>
-//             <div className='row'>
-//               {[
-//                 {
-//                   title: "Bloom and Barter",
-//                   description: "Interactive Full Stack Application",
-//                   image: ,
-//                   deployedLink: ,
-//                   githubLink: ,
-//                 }
-//                 {
-//                   title: ,
-//                   description: ,
-//                   image: ,
-//                   deployedLink: ,
-//                   githubLink: ,
-//                 }
-//                 {
-//                   title: ,
-//                   description: ,
-//                   image: ,
-//                   deployedLink: ,
-//                   githubLink: ,
-//                 }
-//                 {
-//                   title: ,
-//                   description: ,
-//                   image: ,
-//                   deployedLink: ,
-//                   githubLink: ,
-//                 }
-//                 {
-//                   title: ,
-//                   description: ,
-//                   image: ,
-//                   deployedLink: ,
-//                   githubLink: ,
-//                 }
-//                 {
-//                   title: ,
-//                   description: ,
-//                   image: ,
-//                   deployedLink: ,
-//                   githubLink: ,
-//                 }
-//               ]}
-//         </div>
-//         </div>
-//         </div>
-//       </section>
-//   )
-// }
+const Portfolio = () => {
+const projects = [
+  {
+      title: "Bloom and Barter",
+      description: "Interactive Full Stack Application",
+      image: "../../assets/images/BloomAndBarter.png",
+      deployedSite: "https://bloom-and-barter-3911749eadd3.herokuapp.com",
+      github: "https://github.com/jnaoroji/Bloom_and_Barter",
+  },
+  {
+      title: "MVC Tech Blog",
+      description: "Model-View-Controller (MVC): Tech Blog",
+      image: "mvc-tech-blog",
+      deployedSite: "https://still-headland-43283-de293bb87f62.herokuapp.com/",
+      github: "https://github.com/mcramileux/mcramileux-tech-blog",
+  },
+  {
+      title: "McramileuxCssPortfolio",
+      description: "Advanced CSS: Portfolio",
+      image: "McramileuxCssPortfolio",
+      deployedSite: "https://mcramileux.github.io/Kristines-Portfolio/",
+      github: "https://github.com/mcramileux/Kristines-Portfolio",
+  },
+  {
+      title: "NoteTaker",
+      description: "Express.js: Note Taker",
+      image: "NoteTaker",
+      deployedSite: "https://rocky-fjord-38162.herokuapp.com/",
+      github: "https://github.com/mcramileux/mcramileux-note-taker",
+  },
+  {
+      title: "DailyScheduler",
+      description: "Third-Party APIs: Work Day Scheduler",
+      image: "DailyScheduler",
+      deployedSite: "https://mcramileux.github.io/kristines-daily-routine-scheduler/",
+      github: "https://github.com/mcramileux/kristines-daily-routine-scheduler",
+  },
+  {
+      title: "WeatherDashboard",
+      description: "Server-Side APIs: Weather Dashboard",
+      image: "WeatherDashboard",
+      deployedSite: "https://mcramileux.github.io/kristines-weather-dashboard/",
+      github: "https://github.com/mcramileux/kristines-weather-dashboard",
+  },
+]
 
-  
- 
+return (
+  <section className='container-portfolio my-5'>
+    <h1 className='portfolio-title'>Portfolio</h1>
+      <div className='container-portfolio text-center'></div>
+    <div className='row'>
+      {projects.map((project, index) => (
+       <Project project = {project} key= {'project'+ index}/>
+      ))}
+    </div>
+  </section>
+);
+};
+
+export default Portfolio;
---
+++ mcramileux-portfolio/src/components/pages/Project.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Project({project}) {
+    const {title, description, image, deployedSite, github} = project;
+
+    return (
+        <div key={title} className="col-lg-4 col-md-6 mb-4">
+        <div className="card h-100">
+          <img src={image} className="card-img-top" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+          </div>
+          <div className="card-footer">
+            <a href={github} target="_blank" rel="noreferrer">
+              GitHub
+            </a>
+          </div>
+          <div>
+            <a href={deployedSite} target="_blank" rel="noreferrer">
+              Deployed Link
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  export default Project;
